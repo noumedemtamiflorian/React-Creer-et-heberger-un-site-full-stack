627f8770cd3922db442fc470b6e90be9d2769441
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-01-02 05:42:08 +0100

    Mettre en place une base de donnees avec MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 import express from "express"
+import { MongoClient, ServerApiVersion } from "mongodb"
+import dotenv from "dotenv"
+
 const app = express();
 const port = 4000
+dotenv.config()
+
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    // const collection = client.db("test").collection("devices");
+    // // perform actions on the collection object
+    console.log("Connecte avec succes a la base de donnee");
+    client.close();
+});
+
 
 app.get("/", (_, res) => {
     res.send("Hello Express")
