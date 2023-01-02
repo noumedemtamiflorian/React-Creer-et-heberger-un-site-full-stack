7bbc9874e9da06f6d956194b69440cad4bc8d027
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-01-02 04:58:46 +0100

    Relancer le serveur automatiquement

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express"
 const app = express();
 const port = 4000
 
+app.get("/", (_, res) => {
+    res.send("Hello Express")
+})
+
 app.listen(port, () => {
     console.log("Serveur demarre avec success sur le port 4000");
 }) 
