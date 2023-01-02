f07520cd293bafbba2f9651654b440dcc8bcd1c7
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-01-02 04:52:49 +0100

    Installer le compilateur Babel

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+import express from "express"
 const app = express();
 const port = 4000
 
 app.listen(port, () => {
     console.log("Serveur demarre avec success sur le port 4000");
-})
+}) 
