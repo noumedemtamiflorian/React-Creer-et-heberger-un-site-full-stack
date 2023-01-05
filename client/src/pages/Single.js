a72aa7663b94ca9778734d2f8cfde3e283bc61f0
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-01-05 20:42:37 +0100

    Partager un état global avec les composants de l'application

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { useLocation } from "react-router-dom"
-import data from "../data"
+import { useAppContext } from "../context"
 
 export default function Single() {
+    const { posts } = useAppContext()
     const params = useLocation()
-    const post = data.find(post => post._id === params?.state?.id)
+    const post = posts.find(post => post._id === params?.state?.id)
     return (<>
         <h1>{post?.title}</h1>
         <p>{post?.content}</p>
